37278142704b2b11bb5d8ce26dfc4bcdcf38b103
Alamega <vladshuman@gmail.com>
2023-12-21 10:22:46 +0300

    Готово

--- src/main/java/com/example/graf/GrafApplication.java
+++ src/main/java/com/example/graf/GrafApplication.java
@@ -3,9 +3,11 @@ package com.example.graf;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class GrafApplication extends Application {
     @Override
@@ -16,6 +18,7 @@ public class GrafApplication extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(GrafApplication.class.getResource("graf-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Расчет минимального пути графа метрополитена СПб");
+        stage.getIcons().add(new Image(Objects.requireNonNull(GrafApplication.class.getResourceAsStream("/logo.png"))));
         stage.setScene(scene);
         stage.show();
     }
